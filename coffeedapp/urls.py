d53355b5e8fc76c0eb3993591462ad49d53de7f0
varveler <evarval@gmail.com>
2015-04-09 23:56:13 -0500

    aded import

--- coffeedapp/urls.py
+++ coffeedapp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import core.views as coreviews
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,5 +8,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', core.views.SplashView.as_view()),
+    url(r'^$', coreviews.SplashView.as_view()),
 )
